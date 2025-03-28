93d7d412970e243d3c8fde4f39dc5615901a0667
daniel.nunessilva <danielzcomputacao@gmail.com>
2025-03-28 14:56:09 -0300

    feat: Creating Nextjs project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="h-screen w-full flex justify-center items-center">
+            Website in development
+        </div>
+    );
+};
+
+export default Home;
