a17b6ad7aa91a2e66bc1c42e060d57647cf2e024
daniel.nunessilva <danielzcomputacao@gmail.com>
2025-03-28 15:54:10 -0300

    test: testing CI/CD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div className="h-screen w-full flex justify-center items-center text-white">
+        <div className="h-screen w-full flex justify-center items-center">
             Website in development
         </div>
     );
