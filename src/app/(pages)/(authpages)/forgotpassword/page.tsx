d70e3e93c58192fffbc492c0046d2639b1abac11
daniel.nunessilva <danielzcomputacao@gmail.com>
2025-05-09 16:58:00 -0300

    ForgotPassword is adapted to the theme

--- src/app/(pages)/(authpages)/forgotpassword/page.tsx
+++ src/app/(pages)/(authpages)/forgotpassword/page.tsx
@@ -14,22 +14,42 @@ export default function ForgotPasswordPage() {
     if (sendEmail) {
         return (
             <>
-                <div className="md:w-1/2 flex justify-center items-center flex-col bg-[#071717] p-10 space-y-4">
+                <div
+                    className={`md:w-1/2 flex justify-center items-center flex-col p-10 space-y-4  ${
+                        theme == "light"
+                            ? "bg-gray-200 text-black"
+                            : "bg-[#071717] text-white"
+                    }`}
+                >
                     <div className=" space-y-4 w-full">
-                        <span className="text-[#59cb75] text-2xl font-bold">
+                        <span
+                            className={`${
+                                theme == "light"
+                                    ? "text-[#189938]"
+                                    : "text-[#34C759]"
+                            } text-2xl font-bold`}
+                        >
                             Email enviado com sucesso! Verifique sua caixa de
                             entrada ou spam.
                         </span>
                         <button
                             onClick={() => redirect("/login")}
-                            className="w-full cursor-pointer py-2 mt-16 bg-[#34C759] hover:bg-[#1E842D] rounded text-white"
+                            className={`w-full cursor-pointer py-2 mt-16 rounded text-white ${
+                                theme == "light"
+                                    ? "bg-[#34C759] hover:bg-[#1E842D]"
+                                    : "bg-[#1E842D] hover:bg-[#34C759]"
+                            }`}
                         >
                             voltar
                         </button>
                     </div>
                 </div>
 
-                <div className="md:w-1/2  bg-[#59cb75] text-[#0A1E1E] p-10 flex flex-col justify-between items-center">
+                <div
+                    className={`md:w-1/2 ${
+                        theme == "light" ? "bg-[#59cb75]" : "bg-[#1E842D]"
+                    } text-[#0A1E1E] p-10 flex flex-col justify-between items-center`}
+                >
                     <Image
                         src={"/forgotpassword.png"}
                         width={400}
@@ -43,10 +63,22 @@ export default function ForgotPasswordPage() {
 
     return (
         <>
-            <div className="md:w-1/2 flex items-center  justify-center flex-col bg-[#071717] p-10 space-y-4">
+            <div
+                className={`md:w-1/2 flex items-center  justify-center flex-col  ${
+                    theme == "light"
+                        ? "bg-gray-200 text-black"
+                        : "bg-[#071717] text-white"
+                } p-10 space-y-4`}
+            >
                 <h1 className="text-2xl font-bold">Esqueceu sua senha?</h1>
                 <span>
-                    <span className="text-[#59cb75]">
+                    <span
+                        className={`${
+                            theme == "light"
+                                ? "text-[#189938]"
+                                : "text-[#34C759]"
+                        }`}
+                    >
                         Não se preocupe! Isso acontece.
                     </span>{" "}
                     Por favor, adicione abaixo o e-mail associado a sua conta
@@ -56,11 +88,17 @@ export default function ForgotPasswordPage() {
                     <input
                         type="email"
                         placeholder="E-mail"
-                        className="w-full px-4 py-2 rounded bg-white text-[#0A1E1E] outline-none"
+                        className={`w-full px-4 py-2 rounded  outline-none  ${
+                            theme == "light" ? "bg-white " : " bg-[#072727]"
+                        }`}
                     />
                     <button
                         onClick={() => handleClick()}
-                        className="w-full py-2 cursor-pointer bg-[#34C759] hover:bg-[#1E842D] rounded text-white"
+                        className={`w-full py-2 cursor-pointer rounded text-white ${
+                            theme == "light"
+                                ? "bg-[#34C759] hover:bg-[#1E842D]"
+                                : "bg-[#1E842D] hover:bg-[#34C759]"
+                        }`}
                     >
                         Enviar
                     </button>
